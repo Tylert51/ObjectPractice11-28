93131583e7607d732ef3cf57358081f9bb8fdbd3
tyler <ttang7910@bths.edu>
2022-11-28 12:34:03 -0500

    working on person class

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Person(String fName, String lName) {
+        firstName = fName;
+        lastName = lName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void changeEmail(String newEmail) {
+        newEmail = email;
+    }
+}
